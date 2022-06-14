c48d3e77c250b4ea6907a54241ebff235527093e
josiah <jgarrison12@hotmail.com>
2022-06-13 21:22:18 -0600

    deploy to gh pages

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NotesIcon from '@mui/icons-material/Notes';
+
+function Header() {
+  return <header>
+    <h1>Memocho <NotesIcon /></h1>
+  </header>
+}
+
+export default Header;
